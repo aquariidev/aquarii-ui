1f7cc8eedd404217b211c05fa9165b7317f070fb
mlazuardy <lazuardymichael@gmail.com>
2020-10-20 16:29:39 +0700

    remove console.log

--- packages/ui/nuxt.js
+++ packages/ui/nuxt.js
@@ -2,7 +2,6 @@ import { join } from 'path'
 
 export default function () {
   this.nuxt.hook('components:dirs', (dirs) => {
-    console.log(dirs);
     dirs.push({
       path: join(__dirname, 'src/components'),
       prefix: 'aq'
