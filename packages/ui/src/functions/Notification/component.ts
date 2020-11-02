3d9ad212ef6f1fc2226bdb5432ada14cf66004e6
mlazuardy <lazuardymichael@gmail.com>
2020-11-02 14:32:27 +0700

    add

--- packages/ui/src/functions/Notification/component.ts
+++ packages/ui/src/functions/Notification/component.ts
@@ -19,9 +19,12 @@ export default class AQNotification extends Vue {
   /** Mounted */
   mounted(): void {
     this.isShow = true;
-    setTimeout(() => {
-      this.close();
-    }, this.timeOut);
+
+    if(this.timeOut) {
+      setTimeout(() => {
+        this.close();
+      }, this.timeOut);
+    }
   }
 
   /** Close Notification */
@@ -56,28 +59,29 @@ export default class AQNotification extends Vue {
 
   /** Render element */
   public render(h: CreateElement): VNode {
-    const title = h('p', {
-      class: 'text-sm leading-5 font-medium text-gray-900',
+    const title = h('div', {
+      class: 'aq-title',
       domProps: {
         innerHTML: this.title,
       }
     });
 
     const message = h('p', {
-      class: `${this.title ? 'mt-1' : ''} text-sm leading-5 text-gray-500`,
+      class: 'aq-message',
       domProps: {
         innerHTML: this.message
       }
     })
 
     const body = h('div', {
-      class: 'ml-3 w-0 flex-1 pt-0.5'
+      class: 'aq-body'
     }, [
-      title, message
+      this.title && title,
+      message
     ]);
 
     const closeButton = h('button', {
-      class: 'inline-flex text-gray-400 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150',
+      class: 'aq-close',
       on: {
         click: this.close
       }
@@ -90,32 +94,24 @@ export default class AQNotification extends Vue {
     ])
 
     const closeButtonContent = h('div', {
-      class: 'ml-4 flex-shrink-0 flex'
+      class: 'aq-close-content'
     }, [closeButton])
 
     const innerDialog = h('div', {
-      class: 'rounded-lg shadow-xs overflow-hidden p-4 flex items-start',
+      class: 'aq-inner-dialog',
       key: Math.random().toString(),
     }, [body, closeButtonContent])
 
-    // const dialog = h('div', {
-    //   staticClass: 'aq-dialog',
-      // class: this.notificationClass,
-      // key: Math.random().toString()
-    // }, [innerDialog]);
-
-    return h('transition-group', {
-      tag: 'div',
+    const dialog = h('div', {
       staticClass: 'aq-dialog',
       class: this.notificationClass,
+      key: Math.random().toString()
+    }, [innerDialog]);
+
+    return h('transition', {
       props: {
-        'enter-active-class': 'transform ease-out duration-300 transition',
-        'enter-class': 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2',
-        'enter-to-class': 'translate-y-0 opacity-100 sm:translate-x-0',
-        'leave-active-class': 'transition ease-in duration-100',
-        'leave-class': 'opacity-100',
-        'leave-to-class': 'opacity-0',
+        name: 'aq-notification'
       }
-    }, [this.isShow && innerDialog])
+    }, [this.isShow && dialog])
   }
 }
