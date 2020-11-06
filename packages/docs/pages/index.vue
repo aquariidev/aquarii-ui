9b4d6c5343ee04d6ca33809ad1b9c20895471078
mlazuardy <lazuardymichael@gmail.com>
2020-11-06 16:01:10 +0700

    add

--- packages/docs/pages/index.vue
+++ packages/docs/pages/index.vue
@@ -11,7 +11,7 @@
                   <span class="text-indigo-400">Vue</span>
                 </h2>
                 <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl text-center">
-                  Aquarii is a simple Vue Component Framework that help you to create a <strike>Simple</strike> <b>Good</b> Applications
+                  Aquarii is a simple Vue Component Framework that help you to create a <strike><b>Bad</b></strike> <b>Good</b> Applications
                 </p>
                 <div class="mt-5 text-center">
                   <aq-button color="primary" @click="$router.push('/docs')" size="large">Documentation</aq-button>
